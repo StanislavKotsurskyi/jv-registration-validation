3be15c25d322fe59bdac791f1a4b96498c5317b4
Stanislav <Sergie3005@gmail.com>
2026-06-04 16:05:30 +0300

    testsssssssss

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,6 +2,7 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exception.InvalidDataException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
@@ -9,6 +10,26 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new InvalidDataException("User can`t be null");
+        }
+        if (user.getLogin() == null
+                || user.getPassword() == null
+                || user.getAge() == null) {
+            throw new InvalidDataException("User fields cannot be null");
+        }
+        if (user.getLogin().length() < 6) {
+            throw new InvalidDataException("User login length must be at least 6 characters");
+        }
+        if (user.getPassword().length() < 6) {
+            throw new InvalidDataException("User password length must be at least 6 characters");
+        }
+        if (user.getAge() < 18) {
+            throw new InvalidDataException("User must be at least 18 years old");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new InvalidDataException("User already exists");
+        }
+        return storageDao.add(user);
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,109 @@
+package core.basesyntax.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import core.basesyntax.exception.InvalidDataException;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceImplTest {
+
+    @Test
+    void wrongAge_notOk() {
+        User user = new User();
+        user.setAge(15);
+        user.setLogin("dasersix11");
+        user.setPassword("1234565");
+        RegistrationServiceImpl reg = new RegistrationServiceImpl();
+        assertThrows(InvalidDataException.class, () -> reg.register(user));
+    }
+
+    @Test
+    void exactAge_Ok() {
+        User user = new User();
+        user.setAge(18);
+        user.setLogin("dasersix45");
+        user.setPassword("12345665");
+        RegistrationServiceImpl reg = new RegistrationServiceImpl();
+        User result = reg.register(user);
+        assertNotNull(result);
+        assertEquals(18, result.getAge());
+    }
+
+    @Test
+    void wrongPassword_notOk() {
+        User user = new User();
+        user.setAge(19);
+        user.setLogin("sazadi4");
+        user.setPassword("12345");
+        RegistrationServiceImpl reg = new RegistrationServiceImpl();
+        assertThrows(InvalidDataException.class, () -> reg.register(user));
+    }
+
+    @Test
+    void exactPassword_Ok() {
+        User user = new User();
+        user.setAge(19);
+        user.setLogin("sazadi75");
+        user.setPassword("123456");
+        RegistrationServiceImpl reg = new RegistrationServiceImpl();
+        User result = reg.register(user);
+        assertNotNull(result);
+        assertEquals("123456", result.getPassword());
+    }
+
+    @Test
+    void wrongLogin_notOk() {
+        User user = new User();
+        user.setAge(19);
+        user.setLogin("daser");
+        user.setPassword("12345678");
+        RegistrationServiceImpl reg = new RegistrationServiceImpl();
+        assertThrows(InvalidDataException.class, () -> reg.register(user));
+    }
+
+    @Test
+    void exactLogin_Ok() {
+        User user = new User();
+        user.setAge(19);
+        user.setLogin("dasers");
+        user.setPassword("12345678");
+        RegistrationServiceImpl reg = new RegistrationServiceImpl();
+        User result = reg.register(user);
+        assertNotNull(result);
+        assertEquals("dasers", result.getLogin());
+    }
+
+    @Test
+    void emptyUser_notOk() {
+        User user = new User();
+        RegistrationServiceImpl reg = new RegistrationServiceImpl();
+        assertThrows(InvalidDataException.class, () -> reg.register(user));
+    }
+
+    @Test
+    void userExists_notOk() {
+        assertThrows(InvalidDataException.class, () -> {
+            User user = new User();
+            user.setAge(19);
+            user.setLogin("dasersix");
+            user.setPassword("12345678");
+            RegistrationServiceImpl reg = new RegistrationServiceImpl();
+            reg.register(user);
+            reg.register(user);
+        });
+    }
+
+    @Test
+    void nullUser_notOk() {
+        User user = new User();
+        user.setAge(null);
+        user.setId(null);
+        user.setLogin(null);
+        user.setPassword(null);
+        RegistrationServiceImpl reg = new RegistrationServiceImpl();
+        assertThrows(InvalidDataException.class, () -> reg.register(user));
+    }
+}
